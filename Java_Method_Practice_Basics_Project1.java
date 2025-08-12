875f6e483bc41d6253835b293ab48fc786b37f5b
unknownk0420 <unknwonk0420@gmail.com>
2025-08-12 15:44:31 +0900

    Java_Method_Project1

---
+++ Java_Method_Practice_Basics_Project1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Java_Method_Practice_Basics_Project1 {
+    public static int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static int subtract(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public static int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public static double divide(int number1, int number2) {
+        if (number2 == 0) {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return 0;
+        } else {
+            return (double) number1 / number2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력해 주세요.: ");
+        int num1 = sc.nextInt();
+        System.out.print("연산자(+, -, *, /)를 입력해 주세요.: ");
+        String mark = sc.next();
+        System.out.print("두 번째 숫자를 입력해 주세요.:");
+        int num2 = sc.nextInt();
+        switch (mark) {
+            case "+":
+                System.out.println(add(num1, num2));
+                break;
+            case "-":
+                System.out.println(subtract(num1, num2));
+                break;
+            case "*":
+                System.out.println(multiply(num1, num2));
+                break;
+            default:
+                System.out.println(divide(num1, num2));
+                break;
+        }
+    }
+}
